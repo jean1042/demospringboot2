70ada99db840d51492c84a1653e6bf50f5fa1acd
June <jean1042@naver.com>
2024-04-27 09:56:04 +0900

    Update get mapping

--- src/main/java/com/example/demo3/Demo3Application.java
+++ src/main/java/com/example/demo3/Demo3Application.java
@@ -3,16 +3,20 @@ package com.example.demo3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class Demo3Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Demo3Application.class, args);
 	}
 
-	@GetMapping("/hello")
+	@RequestMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "June") String name) {
-		return String.format("Juneee2 Hello %s!", name);
+		return String.format("Juneee3 Hello %s!", name);
 	}
 }
