ddcd7de6c0aa16eaaf91477af2e69ccefd23f5b8
June <jean1042@naver.com>
2024-04-28 20:08:09 +0900

    Add post method

--- src/main/java/com/example/demo3/Demo3Application.java
+++ src/main/java/com/example/demo3/Demo3Application.java
@@ -2,10 +2,11 @@ package com.example.demo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @SpringBootApplication
@@ -15,8 +16,13 @@ public class Demo3Application {
 		SpringApplication.run(Demo3Application.class, args);
 	}
 
-	@RequestMapping("/hello")
+	@RequestMapping("/getuser")
 	public String hello(@RequestParam(value = "name", defaultValue = "June") String name) {
 		return String.format("Juneee3 Hello %s!", name);
+	}
+
+	@PostMapping("/postuser")
+	public String postTest2(@RequestBody String username){
+		return "User created with username: " + username;
 	}
 }
---
+++ src/main/java/com/example/demo3/SearchParam.java
@@ -0,0 +1,10 @@
+package com.example.demo3;
+
+
+public class SearchParam {
+    private String username;
+
+//    public String postTest(){
+//        this.username = username;
+//    }
+}
